0a891957d4a6e2afc4176b555fc790d4c5bacd05
Vladimir Panteleev <git@cy.md>
2022-01-05 00:06:44 +0000

    Work around format-mode-line issues
    - Catch errors that it seems to sometimes throw. Emacs bug?
    
    - Add window-state-change-hook in additino to post-command-hook

--- term-title.el
+++ term-title.el
@@ -1,6 +1,6 @@
 ;;; term-title.el --- Synchronize Emacs frame title with terminal title
 
-;; Copyright (C) 2021 Vladimir Panteleev
+;; Copyright (C) 2021, 2022 Vladimir Panteleev
 ;; Copyright (C) 2004 Noah S. Friedman
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -51,25 +51,36 @@ normally, the original title is not restored."
   (if term-title-mode
       (progn
         (add-hook 'post-command-hook 'term-title--update)
+        (add-hook 'window-state-change-hook 'term-title--update)
         (add-hook 'tty-setup-hook 'term-title--update))
     (remove-hook 'post-command-hook 'term-title--update)
+    (remove-hook 'window-state-change-hook 'term-title--update)
     (remove-hook 'tty-setup-hook 'term-title--update)))
 
 (defun term-title--update ()
   "Synchronize terminal window title with the selected Emacs frame, if it is a tty."
-  (when (eq (framep-on-display) t)
-    (let ((title (format-mode-line (or term-title-format
-				       (frame-parameter nil 'title)
-				       ;; This would mimic the semantics in X but since
-				       ;; frame ttys always have a name (even if just
-				       ;; the default "F<n>"), don't bother.
-				       ;;
-				       ;;(frame-parameter nil 'name)
-				       frame-title-format)))
-	  (frame (selected-frame)))
-      (unless (string-equal title (frame-parameter frame 'term-title-last-title))
-	(term-title--set title)
-	(set-frame-parameter frame 'term-title-last-title title)))))
+  (when (and (frame-live-p (selected-frame))
+             (eq (framep-on-display) t))
+    (let ((frame (selected-frame))
+          (title-format (or term-title-format
+			    (frame-parameter nil 'title)
+			    ;; This would mimic the semantics in X but since
+			    ;; frame ttys always have a name (even if just
+			    ;; the default "F<n>"), don't bother.
+			    ;;
+			    ;;(frame-parameter nil 'name)
+			    frame-title-format))
+          title)
+      ;; format-mode-line sometimes, for whatever reason, fails with e.g.
+      ;; (wrong-type-argument frame-live-p #<dead frame F23 0x55cbd62f93c0>)
+      (condition-case e
+          (setq title (format-mode-line title-format))
+        (wrong-type-argument
+         (message "format-mode-line error in term-title: %S" e)))
+      (when title
+        (unless (string-equal title (frame-parameter frame 'term-title-last-title))
+	  (term-title--set title)
+	  (set-frame-parameter frame 'term-title-last-title title))))))
 
 (defun term-title--set (title)
   "Unconditionally set the current TTY terminal's title."
